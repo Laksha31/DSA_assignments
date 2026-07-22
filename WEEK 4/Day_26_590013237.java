44b5f0dd6893830f81b3a50ad5e09e2b1a1e7fcf
Laksha31 <kundraalakshaa1@gmail.com>
2026-07-22 22:58:55 +0530

    Day 26 added

---
+++ WEEK 4/Day_26_590013237.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+public class Day_26_590013237 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Node head = null;
+        Node tail = null;
+
+        for (int i = 0; i < n; i++) {
+
+            Node newNode = new Node(sc.nextInt());
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                newNode.prev = tail;
+                tail = newNode;
+            }
+        }
+
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+
+        sc.close();
+    }
+}
